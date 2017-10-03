aee5b9bf68dfe06c06f2846850a3c1b860647d4c
Wagner Mariano <wmariano@id.uff.br>
2017-10-03 15:43:31 -0300

    arrumando código

--- ajuste.c
+++ ajuste.c
@@ -3,10 +3,10 @@
 FILE *arquivo;
 #define n 10
 
-void coeficiente(double x1[n], double y1[n])
+void coeficientes(double x1[n], double y1[n])
 {
     int i;
-    double Sx, Sy, Sxy, Sxx, a0, a1;
+    double Sx, Sy, Sxy, Sxx, a0, a1; 
   	Sx=0;
   	Sy=0;
   	Sxy=0;
@@ -21,29 +21,25 @@ void coeficiente(double x1[n], double y1[n])
 		Sy = Sy + y1[i];
 	}
 	
-	//Imprimindo os somatórios
-	{
-		printf("\nSx = %.1lf\nSy = %.1lf\nSxx = %.1lf\nSxy = %.1lf\n\n", Sx, Sy, Sxx, Sxy);
-	}
-	
-
-	//determinando os coeficientes a0 e a1
-	
+	//calculando a0 e a1
 	a0 = (Sxx*Sy - Sxy*Sx)/(n*Sxx - Sx*Sx);
 	a1 = (n*Sxy - Sx*Sy)/(n*Sxx - Sx*Sx);
+	
+	printf("Valores de a0 e a1 \n");
 	
 	//Imprimindo a1 e a0
 	printf("\na1 = %lf\na0 = %lf\n\n", a1, a0);
 }
 
 int main()
-{	int i;
+{	
+	int i;
 	double x1[n], y1[n];//coluna 1 e coluna 2 do aquivo "dados.dat"
 	
 	//Abrindo o arquivo para leitura
 	arquivo = fopen("dados.dat", "r");
 	
-	//Lendo os dados do arquivo
+	//Lendo os dados
 	for(i=0; i<n; i++)
 	{
 		fscanf(arquivo, "%lf\t\t%lf\n", &x1[i], &y1[i]);
@@ -53,11 +49,11 @@ int main()
 	//Imprimindo os dados na tela
 	for(i=0; i<n; i++)
 	{
-		printf("%.1lf\t\t%.1lf\n", x1[i], y1[i]);
+		printf("%.1lf\t%.1lf\n", x1[i], y1[i]);
 	}
 	
 	
-	coeficiente(x1, y1);
+	coeficientes(x1, y1);
 }
 	
 	
